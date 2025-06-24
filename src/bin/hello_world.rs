105915c53f92e9dcf6d5e7b00b27e04e885bcd7f
lurchfresser <Tritrop007@gmx.de>
2025-06-24 16:30:20 +0200

    Ich mag Singen

--- src/bin/hello_world.rs
+++ src/bin/hello_world.rs
@@ -5,6 +5,6 @@ use rouille::Response;
 fn main() {
     println!("Starting server on 0.0.0.0:42069");
     rouille::start_server("0.0.0.0:42069", move |request| {
-        Response::text("hello world")
+        Response::text("hello world 2")
     });
 }
